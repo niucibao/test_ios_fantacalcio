ad1b9c6b320623cd43a588052edee2abcb54ce63
Nicola Lauritano <nlauritano@minsait.com>
2023-06-05 11:39:09 +0200

    Initial Commit

---
+++ Progetto_test_fantacalcio/Progetto_test_fantacalcio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Progetto_test_fantacalcio
+//
+//  Created by Nicola Lauritano on 05/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
